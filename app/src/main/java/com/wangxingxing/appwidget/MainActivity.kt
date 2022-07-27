fb6c533596f4839bb405ebaeb1b7e5afaeed6cc5
xingfengwxx <xingfengwxx@gmail.com>
2022-07-27 20:59:19 +0800

    添加点击事件

--- app/src/main/java/com/wangxingxing/appwidget/MainActivity.kt
+++ app/src/main/java/com/wangxingxing/appwidget/MainActivity.kt
@@ -2,7 +2,12 @@ package com.wangxingxing.appwidget
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.wangxingxing.appwidget.databinding.ActivityMainBinding
+import javax.security.auth.login.LoginException
+
+const val TAG = "wxx"
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +18,17 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val date = intent?.data
+        Log.i(TAG, "onCreate: $date")
+        date?.let {
+            val idRes = Integer.parseInt(it.schemeSpecificPart)
+            Log.i(TAG, "onCreate: $idRes")
+            when(idRes){
+                R.id.iv_big -> {
+                    Toast.makeText(this,"点击大图", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/wangxingxing/appwidget/TodoWidget.kt
+++ app/src/main/java/com/wangxingxing/appwidget/TodoWidget.kt
@@ -1,10 +1,14 @@
 package com.wangxingxing.appwidget
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.util.Log
 import android.widget.RemoteViews
+import androidx.annotation.IdRes
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.AppWidgetTarget
 
@@ -43,16 +47,33 @@ internal fun updateAppWidget(
     val views = RemoteViews(context.packageName, R.layout.todo_widget)
     views.setTextViewText(R.id.appwidget_text, widgetText)
 
-    Log.i("wxx", "updateAppWidget: start")
-    val appWidgetTarget = AppWidgetTarget(context, 100, 100, R.id.iv_big, views, R.id.iv_big)
+    Log.i(TAG, "updateAppWidget: start")
+    val appWidgetTarget = AppWidgetTarget(context, 200, 200, R.id.iv_big, views, R.id.iv_big)
     Glide.with(context.applicationContext)
         .asBitmap()
         .load("https://cdn.sbnh.cn/ic_cool.png")
         .into(appWidgetTarget)
-    Log.i("wxx", "updateAppWidget: end")
+    Log.i(TAG, "updateAppWidget: end")
 
 //    views.setImageViewResource(R.id.iv_big, R.mipmap.ic_hamburger)
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
+
+    views.setOnClickPendingIntent(R.id.iv_big, openAppPendingIntent(context, R.id.iv_big))
+}
+
+private fun openAppPendingIntent(context: Context,@IdRes id:Int):PendingIntent{
+    /*打开APP intent*/
+    val activityIntent = Intent(context, MainActivity::class.java).apply {
+        setData(Uri.parse("harvic:$id"))
+        flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+    }
+    val appOpenIntent = PendingIntent.getActivity(
+        context,
+        1,
+        activityIntent,
+        PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_IMMUTABLE
+    )
+    return appOpenIntent
 }
